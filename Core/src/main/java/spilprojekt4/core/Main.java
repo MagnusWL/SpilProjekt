0e305b7afaf9e1e2d2e320ad3d439204a3137576
Michael-PC <Michael-PC@DESKTOP-OJVKIFN>
2017-02-22 14:10:44 +0100

    Merge origin/master
    Conflicts:
    	SemesterProject4/src/semesterproject4/fuck.java

--- Core/src/main/java/spilprojekt4/core/Main.java
+++ Core/src/main/java/spilprojekt4/core/Main.java
@@ -16,13 +16,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 
     public static void main(String[] args) {
-        LwjglApplicationConfiguration cfg = 
-                new LwjglApplicationConfiguration();
-        
+        LwjglApplicationConfiguration cfg
+                = new LwjglApplicationConfiguration();
+
         cfg.title = "2D shooter game";
         cfg.width = 1280;
         cfg.height = 720;
-        
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+
         new LwjglApplication(new Game(), cfg);
     }
 }
