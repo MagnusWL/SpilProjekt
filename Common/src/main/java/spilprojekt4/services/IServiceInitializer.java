1a579b81fad48ea46f97163a61e62e30ec08e37c
Michael-PC <Michael-PC@DESKTOP-OJVKIFN>
2017-02-22 12:35:36 +0100

    Merge origin/master
    Conflicts:
    	SemesterProject4/src/semesterproject4/fuck.java

---
+++ Common/src/main/java/spilprojekt4/services/IServiceInitializer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spilprojekt4.services;
+
+/**
+ *
+ * @author Michael-PC
+ */
+public interface IServiceInitializer {
+    
+}
