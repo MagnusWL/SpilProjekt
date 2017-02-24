1fc3cf7e60d05a20ff83bf8e040f9cbfc2f0e1b4
Michael-PC <Michael-PC@DESKTOP-OJVKIFN>
2017-02-24 14:45:52 +0100

    Merge origin/master
    Conflicts:
    	Common/src/main/java/spilprojekt4/common/Entity.java

--- Core/src/main/java/spilprojekt4/core/InputController.java
+++ Core/src/main/java/spilprojekt4/core/InputController.java
@@ -6,67 +6,69 @@ import spilprojekt4.common.GameData;
 import spilprojekt4.common.GameKeys;
 
 public class InputController extends InputAdapter {
-    
+
     private final GameData gameData;
 
     public InputController(GameData gameData) {
         this.gameData = gameData;
     }
 
-	public boolean keyDown(int k) {
-		if(k == Input.Keys.W) {
-                    gameData.getKeys().setKey(GameKeys.W, true);
-		}
-		if(k == Input.Keys.A) {
-                    gameData.getKeys().setKey(GameKeys.A, true);
-		}
-		if(k == Input.Keys.S) {
-                    gameData.getKeys().setKey(GameKeys.S, true);
-		}
-		if(k == Input.Keys.D) {
-                    gameData.getKeys().setKey(GameKeys.D, true);
-		}
-		if(k == Input.Keys.ENTER) {
-                    gameData.getKeys().setKey(GameKeys.ENTER, true);
-		}
-		if(k == Input.Keys.ESCAPE) {
-                    gameData.getKeys().setKey(GameKeys.ESCAPE, true);
-		}
-		if(k == Input.Keys.SPACE) {
-                    gameData.getKeys().setKey(GameKeys.SPACE, true);
-		}
-		if(k == Input.Keys.SHIFT_LEFT || k == Input.Keys.SHIFT_RIGHT) {
-                    gameData.getKeys().setKey(GameKeys.SHIFT, true);
-		}
-		return true;
-	}
-	
-	public boolean keyUp(int k) {
-		if(k == Input.Keys.W) {
-                    gameData.getKeys().setKey(GameKeys.W, false);
-		}
-		if(k == Input.Keys.A) {
-                    gameData.getKeys().setKey(GameKeys.A, false);
-		}
-		if(k == Input.Keys.S) {
-                    gameData.getKeys().setKey(GameKeys.S, false);
-		}
-		if(k == Input.Keys.D) {
-                    gameData.getKeys().setKey(GameKeys.D, false);
-		}
-		if(k == Input.Keys.ENTER) {
-                    gameData.getKeys().setKey(GameKeys.ENTER, false);
-		}
-		if(k == Input.Keys.ESCAPE) {
-                    gameData.getKeys().setKey(GameKeys.ESCAPE, false);
-		}
-		if(k == Input.Keys.SPACE) {
-                    gameData.getKeys().setKey(GameKeys.SPACE, false);
-		}
-		if(k == Input.Keys.SHIFT_LEFT || k == Input.Keys.SHIFT_RIGHT) {
-                    gameData.getKeys().setKey(GameKeys.SHIFT, false);
-		}
-		return true;
-	}
-	
+    @Override
+    public boolean keyDown(int k) {
+        if (k == Input.Keys.W) {
+            gameData.getKeys().setKey(GameKeys.W, true);
+        }
+        if (k == Input.Keys.A) {
+            gameData.getKeys().setKey(GameKeys.A, true);
+        }
+        if (k == Input.Keys.S) {
+            gameData.getKeys().setKey(GameKeys.S, true);
+        }
+        if (k == Input.Keys.D) {
+            gameData.getKeys().setKey(GameKeys.D, true);
+        }
+        if (k == Input.Keys.ENTER) {
+            gameData.getKeys().setKey(GameKeys.ENTER, true);
+        }
+        if (k == Input.Keys.ESCAPE) {
+            gameData.getKeys().setKey(GameKeys.ESCAPE, true);
+        }
+        if (k == Input.Keys.SPACE) {
+            gameData.getKeys().setKey(GameKeys.SPACE, true);
+        }
+        if (k == Input.Keys.SHIFT_LEFT || k == Input.Keys.SHIFT_RIGHT) {
+            gameData.getKeys().setKey(GameKeys.SHIFT, true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int k) {
+        if (k == Input.Keys.W) {
+            gameData.getKeys().setKey(GameKeys.W, false);
+        }
+        if (k == Input.Keys.A) {
+            gameData.getKeys().setKey(GameKeys.A, false);
+        }
+        if (k == Input.Keys.S) {
+            gameData.getKeys().setKey(GameKeys.S, false);
+        }
+        if (k == Input.Keys.D) {
+            gameData.getKeys().setKey(GameKeys.D, false);
+        }
+        if (k == Input.Keys.ENTER) {
+            gameData.getKeys().setKey(GameKeys.ENTER, false);
+        }
+        if (k == Input.Keys.ESCAPE) {
+            gameData.getKeys().setKey(GameKeys.ESCAPE, false);
+        }
+        if (k == Input.Keys.SPACE) {
+            gameData.getKeys().setKey(GameKeys.SPACE, false);
+        }
+        if (k == Input.Keys.SHIFT_LEFT || k == Input.Keys.SHIFT_RIGHT) {
+            gameData.getKeys().setKey(GameKeys.SHIFT, false);
+        }
+        return true;
+    }
+
 }
