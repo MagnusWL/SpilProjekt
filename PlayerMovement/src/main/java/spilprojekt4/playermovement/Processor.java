ab0657febd125ccf1d846603efff5b369002f11f
Michael-PC <Michael-PC@DESKTOP-OJVKIFN>
2017-02-24 14:14:01 +0100

    Merge origin/master
    Conflicts:
    	SemesterProject4/src/semesterproject4/fuck.java

--- PlayerMovement/src/main/java/spilprojekt4/playermovement/Processor.java
+++ PlayerMovement/src/main/java/spilprojekt4/playermovement/Processor.java
@@ -1,5 +1,7 @@
 package spilprojekt4.playermovement;
 
+import spilprojekt4.common.Entity;
+import spilprojekt4.common.EntityType;
 import spilprojekt4.common.GameData;
 import spilprojekt4.common.World;
 import spilprojekt4.common.services.IServiceProcessor;
@@ -8,6 +10,25 @@ public class Processor implements IServiceProcessor {
 
     @Override
     public void process(GameData gameData, World world) {
-        
+
+        for (Entity entity : world.getEntities(EntityType.PlAYER)) {
+            
+            if (gameData.getKeys().isDown(1)) {
+                //left
+
+            }
+            if (gameData.getKeys().isDown(3)) {
+                //right
+
+            }
+            if (gameData.getKeys().isDown(6)) {
+                //space
+
+            }
+            
+            entity.getX();
+            entity.getY();
+        }
+
     }
 }
