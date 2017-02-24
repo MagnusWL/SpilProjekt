f459bf8460fb50361d62d7960800fe414646e7c3
Michael-PC <Michael-PC@DESKTOP-OJVKIFN>
2017-02-24 14:56:15 +0100

    Merge origin/master
    Conflicts:
    	Common/src/main/java/spilprojekt4/common/Entity.java

--- Gravity/src/main/java/spilprojekt4/gravity/Processor.java
+++ Gravity/src/main/java/spilprojekt4/gravity/Processor.java
@@ -9,33 +9,16 @@ import static spilprojekt4.common.EntityType.PLAYER;
 import spilprojekt4.common.services.IServiceProcessor;
 
 public class Processor implements IServiceProcessor {
+
     @Override
     public void process(GameData gameData, World world) {
-        
-        for(Entity n : world.getEntities(PLAYER))
-        {
-            if(n.isHasGravity() == true)
-            {
-                /*
-                //force
-                F = m*a
-                
-                F = force
-                m = mass
-                a = acceleration
-                
-                
-                //velocity
-                v = u+a *t
-                u = initial velocity
-                a = acceleration
-                t = time
-                */
-                
-                
-                n.setVelocity(0);
-                
+
+        for (Entity n : world.getEntities(PLAYER)) {
+            if (n.isHasGravity() == true) {
+                n.setVerticalVelocity(n.getVerticalVelocity() + gameData.getGravityConstant());
             }
+            n.setX(n.getX() + n.getVelocity() * gameData.getDelta());
+            n.setY(n.getY() + n.getVerticalVelocity() * gameData.getDelta());
         }
     }
 }
