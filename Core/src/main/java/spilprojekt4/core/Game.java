d5c8d4c5dfc277f9a03c8a68686c927c2f2f1151
Michael-PC <Michael-PC@DESKTOP-OJVKIFN>
2017-02-24 09:17:57 +0100

    Merge origin/master
    Conflicts:
    	SemesterProject4/src/semesterproject4/fuck.java

--- Core/src/main/java/spilprojekt4/core/Game.java
+++ Core/src/main/java/spilprojekt4/core/Game.java
@@ -6,6 +6,9 @@
 package spilprojekt4.core;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import spilprojekt4.common.GameData;
 import spilprojekt4.common.World;
 
@@ -17,6 +20,7 @@ public class Game implements ApplicationListener {
 
     World world;
     GameData gameData;
+    OrthographicCamera cam;
 
     public Game() {
         System.out.println("Game Started");
@@ -24,18 +28,30 @@ public class Game implements ApplicationListener {
 
     @Override
     public void create() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        cam = new OrthographicCamera(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        cam.translate(gameData.getDisplayWidth() / 2, gameData.getDisplayHeight() / 2);
+        cam.update();
+        
     }
 
+    
+    @Override
+    public void render() {
+        
+        // clear screen to black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        gameData.setDelta(Gdx.graphics.getDeltaTime());
+
+    }
+    
     @Override
     public void resize(int i, int i1) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public void render() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     @Override
     public void pause() {
