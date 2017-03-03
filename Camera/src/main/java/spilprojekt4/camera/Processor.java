45693f22f3b9cb06827dd533c27e718498b449d7
Mathias <Mathias@Mathias-Pc>
2017-03-03 11:20:13 +0100

    camera opdatering

--- Camera/src/main/java/spilprojekt4/camera/Processor.java
+++ Camera/src/main/java/spilprojekt4/camera/Processor.java
@@ -10,11 +10,30 @@ public class Processor implements IServiceProcessor {
 
     @Override
     public void process(GameData gameData, World world) {
-        for(Entity player: world.getEntities(EntityType.PLAYER))
-        {
+        for (Entity player : world.getEntities(EntityType.PLAYER)) {
             gameData.setCameraX((int) (player.getX() - gameData.getDisplayWidth() / 2.0));
             gameData.setCameraY((int) (player.getY() - gameData.getDisplayHeight() / 2.0));
+            checkBlankSpace(gameData);
             break;
+        }
+    }
+
+    public void checkBlankSpace(GameData gameData) {
+
+        if (gameData.getCameraX() < 0) {
+            gameData.setCameraX(0);
+        } 
+        
+        else if (gameData.getCameraX() > (gameData.getMapWidth() * gameData.getTileSize()) - gameData.getDisplayWidth()) {
+            gameData.setCameraX((gameData.getMapWidth() * gameData.getTileSize()) - gameData.getDisplayWidth());
+        }
+        
+        if (gameData.getCameraY() > 0) {
+            gameData.setCameraY(0);
+        }
+        
+        else if (gameData.getCameraY() < (gameData.getMapHeight() * gameData.getTileSize()) - gameData.getDisplayHeight()) {
+            gameData.setCameraY((gameData.getMapHeight() * gameData.getTileSize()) - gameData.getDisplayHeight());
         }
     }
 }
